d9c3291e8f1c1d8c2c6226c0f6dfa8ea0c7c18b1
Spike Vinz <spikevinz@gmail.com>
2020-11-02 19:44:57 +0800

    adding tests to game.rb

---
+++ spec/game_spec.rb
@@ -0,0 +1,14 @@
+require '../lib/game.rb'
+
+# trying to figure out how to test this game class and its methods. I don't fucking know what rspec functionalities to use. yet.
+
+describe Game do
+  context '#play' do
+    it 'starts the game loop' do
+      game = Game.new
+
+      expect(game.play).to 
+      
+    end
+  end
+end
