b279e12cff85635d6a9749b5607991896d8b1905
Spike Vinz <spikevinz@gmail.com>
2020-11-12 07:19:30 +0800

    refactoring tests on board_spec.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,45 +1,30 @@
 require '../lib/board.rb'
 
 describe Board do
-  context 'initialize' do
-    it 'creates a board with 9 grid inside an array' do
-      board = Board.new
 
-      expect(board.grid).to eq([
-        ".", ".", ".",
-        ".", ".", ".",
-        ".", ".", "."])
-    end
-  end
+  subject(:board) { described_class.new }
 
   context '#move' do
-    it 'puts a piece to a position' do
-      board = Board.new
-      board.move('O', 1)
 
-      expect(board.grid).to eq([
-        "O", ".", ".",
-        ".", ".", ".",
-        ".", ".", "."])
+    it 'puts a piece to position 1' do
+      player_one = 'O'
+      position = 1
+      grid = board.instance_variable_get(:@grid)
+      expect {board.move(player_one, position)}.to change { grid[position - 1] }.to eq player_one
     end
 
-    it 'detects an existing move' do
-      board = Board.new
+    it "does not allow to move on a same position with twice" do
       board.move('O', 1)
-      
       expect(board.move('O', 1)).to be false
     end
 
     it 'does not allow moves outside the grid' do
-      board = Board.new
-      
       expect(board.move('O', 10)).to be false
     end
   end
 
   context '#pattern?' do
     it 'detects a horizontal pattern' do
-      board = Board.new
       board.move('O', 1)
       board.move('O', 2)
       board.move('O', 3)
@@ -48,7 +33,6 @@ describe Board do
     end
 
     it 'detects a vertical pattern' do
-      board = Board.new
       board.move('O', 1)
       board.move('O', 4)
       board.move('O', 7)
@@ -57,7 +41,6 @@ describe Board do
     end
 
     it 'detects a diagonal pattern' do
-      board = Board.new
       board.move('O', 1)
       board.move('O', 5)
       board.move('O', 9)
@@ -68,7 +51,6 @@ describe Board do
 
     context '#none?' do
       it 'detects wether the board is completely filled without a valid pattern' do
-      board = Board.new
       board.move('X', 1)
       board.move('X', 2)
       board.move('O', 3)
