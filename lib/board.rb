8665ba9a3e368c9c402c3299480e2ca1f2185f05
Spike Vinz <spikevinz@gmail.com>
2020-11-12 07:32:23 +0800

    removing Board#move

--- lib/board.rb
+++ lib/board.rb
@@ -5,14 +5,6 @@ class Board
     @grid = Array.new(9, ".")
   end
 
-  def move(player, position)
-    if valid?(position)
-      @grid[position - 1] = player
-    else
-      false
-    end
-  end
-
   def pattern?(player)
     valid_patterns = [@grid[0], @grid[1], @grid[2]],
                      [@grid[3], @grid[4], @grid[5]],
@@ -33,11 +25,8 @@ class Board
   def show_board
     puts "[#{@grid[0]}] [#{@grid[1]}] [#{@grid[2]}]\n[#{@grid[3]}] [#{@grid[4]}] [#{@grid[5]}]\n[#{@grid[6]}] [#{@grid[7]}] [#{@grid[8]}] \n"
   end
-
-  private
-
   
-  def valid?(pos)
+  def valid_move?(pos)
     @grid[pos - 1] == '.'
   end
 end
