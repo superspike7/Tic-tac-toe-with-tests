f75e249a9b867418ab18dd86b9a5d0a458aa82bf
Spike Vinz <spikevinz@gmail.com>
2020-11-12 07:31:37 +0800

    removing Board#move test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,25 +4,6 @@ describe Board do
 
   subject(:board) { described_class.new }
 
-  context '#move' do
-
-    it 'puts a piece to position 1' do
-      player_one = 'O'
-      position = 1
-      grid = board.instance_variable_get(:@grid)
-      expect {board.move(player_one, position)}.to change { grid[position - 1] }.to eq player_one
-    end
-
-    it "does not allow to move on a same position with twice" do
-      board.move('O', 1)
-      expect(board.move('O', 1)).to be false
-    end
-
-    it 'does not allow moves outside the grid' do
-      expect(board.move('O', 10)).to be false
-    end
-  end
-
   context '#pattern?' do
     it 'detects a horizontal pattern' do
       board.move('O', 1)
